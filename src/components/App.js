2d2efa12a34f2dd8070bcc75efb04b3be3494931
Simon Risberg <sirsimon40@gmail.com>
2021-09-22 23:17:06 +0200

    Moved over all of the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import { Component } from "react";
+import React from "react"
+import ConnectedTodos from "./Todos"
+import ConnectedGoals from "./Goals"
+import { connect } from "react-redux"
+import { handleInitialData } from "../actions/shared"
 
 
-class App extends Component {
-  render () {
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+
+    store.subscribe(() => this.forceUpdate())
+  }
+  render() {
+
+    if (this.props.loading == true) {
+      return <h3>Loading</h3>
+    }
+
     return (
       <div>
-        Hello world!
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
     )
   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
